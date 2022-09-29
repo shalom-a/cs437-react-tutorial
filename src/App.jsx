29661077dbffa7dc8d5328bd8ba390263b97064f
Shalom Alarape <shalomalarape2022@u.northwestern.edu>
2022-09-28 22:29:49 -0500

    task: Banner and CouseList.jsx - updated

--- src/App.jsx
+++ src/App.jsx
@@ -39,20 +39,17 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      </header> */}
+      <div>
         <Banner title={schedule.title}/>
         <CourseList courses={schedule.courses}/>
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
+      </div>
+    </div>
+  );
+};
+{/* <p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -70,10 +67,5 @@ const App = () => {
           >
             Vite Docs
           </a>
-        </p>
-      </header>
-    </div>
-  );
-};
-
+        </p> */}
 export default App;
