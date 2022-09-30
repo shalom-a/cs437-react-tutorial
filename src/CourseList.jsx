18fe23111cd1bede81e4962d4da6dabc00834121
Shalom Alarape <shalomalarape2022@u.northwestern.edu>
2022-09-30 14:38:03 -0500

    task: styling

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,6 +1,17 @@
+import './CourseList.css';
+
 const CourseList = ({courses})=>{
-    return <div>
-        {Object.entries(courses).map((course, i) => <p key={i}>{course[1].term} CS {course[1].number}: {course[1].title}</p>)}
+    return <div className='cards'>
+        {Object.entries(courses).map((course, i) => 
+        <div key={i} className="card">
+            <div className="card-top">
+                <h1>{course[1].term} CS {course[1].number}</h1>
+                <p>{course[1].title}</p>
+            </div>
+            <div className="card-bottom">
+               <p>{course[1].meets}</p> 
+            </div>
+        </div>)}
     </div>
 }
 export default CourseList
