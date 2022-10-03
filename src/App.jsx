5673a738c0152a4dbe1af757d8efbab00784e13c
Shalom Alarape <shalomalarape2022@u.northwestern.edu>
2022-10-02 22:05:51 -0500

    task: fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './Banner';
 import CourseList from './CourseList';
+import {useJsonQuery} from './utilities/fetch'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!data) return <h1>No user data found</h1>;
+
+  return (
+  <>
+    <Banner title={data.title}/>
+    <CourseList courses={data.courses}/>
+  </>)
+}
+
+const queryClient = new QueryClient()
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header> */}
-      <div>
-        <Banner title={schedule.title}/>
-        <CourseList courses={schedule.courses}/>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header> */}
+        <div>
+
+          <Main/>
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 {/* <p>
