6760870c019e8fe83ab0613529d20b3a0a5ad850
Shalom Alarape <shalomalarape2022@u.northwestern.edu>
2022-10-04 23:13:40 -0500

    task: term filter

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,35 @@ import CourseList from './CourseList';
 import {useJsonQuery} from './utilities/fetch'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const selectedCourses = {}
+
+
+const Chooser = ({setQuarter}) => {
+  return (
+    <div>
+      <button onClick={()=>setQuarter('Fall')}>Fall</button>
+      <button onClick={()=>setQuarter('Winter')}>Winter</button>
+      <button onClick={()=>setQuarter('Spring')}>Spring</button>
+    </div>
+    
+  );
+};
+
 const Main = () => {
   const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+  const [quarter, setQuarter] = useState('Fall');
 
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
   if (isLoading) return <h1>Loading user data...</h1>;
   if (!data) return <h1>No user data found</h1>;
 
+  let newdata = Object.values(data.courses).filter((course)=> course.term == quarter)
+
   return (
   <>
     <Banner title={data.title}/>
-    <CourseList courses={data.courses}/>
+    <Chooser setQuarter={setQuarter}/>
+    <CourseList courses={newdata}/>
   </>)
 }
 
